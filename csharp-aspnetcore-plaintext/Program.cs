39e4212a865e733a95f52373a6a8b1ae4c48f639
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-03-12 20:45:41 +0100

    ft: move asp net web files

--- csharp-aspnetcore-plaintext/Program.cs
+++ csharp-aspnetcore-plaintext/Program.cs
@@ -1,6 +1,6 @@
 const int port = 7654;
 var builder = WebApplication.CreateBuilder(args)
-.UseUrls($"http://localhost:{port}");
+    .UseUrls($"http://localhost:{port}");
 var app = builder.Build();
 app.MapGet("/", () => "Hello world!");
 app.Run();
