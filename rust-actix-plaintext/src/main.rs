047190359e1af172439fab16fe77b0263c5a7632
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-03-13 16:28:10 +0100

    ft: add actix plaintext

---
+++ rust-actix-plaintext/src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 7654))?
+    .run()
+    .await
+}
