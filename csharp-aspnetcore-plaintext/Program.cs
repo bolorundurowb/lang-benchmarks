0efa8279d77c8c61f2ebb5a5c54399da813b412f
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-03-14 00:30:22 +0100

    ft: updates and fixes

--- csharp-aspnetcore-plaintext/Program.cs
+++ csharp-aspnetcore-plaintext/Program.cs
@@ -1,8 +1,16 @@
-const int port = 7654;
-var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-var app = builder.Build();
+namespace charp_aspnetcore_plaintext;
 
-app.MapGet("/", () => "Hello world!");
-Console.WriteLine($"Listening on port {port}");
-app.Run($"http://localhost:{port}");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        const int port = 7654;
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello world!");
+        Console.WriteLine($"Listening on port {port}");
+        app.Run($"http://localhost:{port}");
+    }
+}
