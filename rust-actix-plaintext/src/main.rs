55ec0891efa78c6d8033c9850972f3ee064ffe03
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-03-13 17:54:24 +0100

    ft: change artillery config

--- rust-actix-plaintext/src/main.rs
+++ rust-actix-plaintext/src/main.rs
@@ -1,18 +1,15 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Hello world!");
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().route("/", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 7654))?
     .run()
-    .await
+    .await;
 }
